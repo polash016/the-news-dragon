eb6725891c454317cf081482294045e4116dc614
polash016 <robin.shrkr@gmail.com>
2023-04-29 21:00:16 +0600

    categories,rightnav,leftnav

--- src/Shared/LeftNav/LeftNav.jsx
+++ src/Shared/LeftNav/LeftNav.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { ListGroup } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const LeftNav = () => {
+    const [categories, setCategories] = useState([]);
+
+
+    useEffect(() => {
+        fetch('http://localhost:5000/categories')
+        .then(res => res.json())
+        .then(data => setCategories(data))
+    },[])
+
     return (
         <div>
-            left nav
+            <h4>All Caterogy:{categories.length}</h4>
+            <div>
+            {
+                categories.map((category) => <p
+                    key={category.id}>
+                    <Link className='text-decoration-none text-dark mt-2' to={`/category/${category.id}`}>{category.name}</Link>
+                    </p>)
+            }
+            </div>
         </div>
     );
 };
